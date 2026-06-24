958f91bc8de768ac378dff6b5cfd1fc3b22d37db
seguridadmasolcartagena-pixel <seguridadmasolcartagena@gmail.com>
2026-06-24 19:14:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'masol-pci-v8';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './map_b64.js',
+  'https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
